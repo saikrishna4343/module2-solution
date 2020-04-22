0b9907bda4da3c65d48fbc4d467d8719bd77c88f
saikrishna434343 <60270778+saikrishna434343@users.noreply.github.com>
2020-04-22 02:48:01 -0500

    final solution

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ function ToBuyController(ShoppingListCheckOffService){
     try{
       ShoppingListCheckOffService.addItemBought(itemName)
       itemsToBuy.errorMessage=""
+      ShoppingListCheckOffService.checkEmpty()
     }catch(error){
       itemsToBuy.errorMessage= error.message
     }
@@ -24,41 +25,49 @@ AlreadyBoughtController.$inject = ["ShoppingListCheckOffService"]
 function AlreadyBoughtController(ShoppingListCheckOffService){
   let itemsBought= this
 
-  try{
-    itemsBought.items = ShoppingListCheckOffService.getItems("itemsBought")
-    debugger
-  }catch(error){
-    itemsBought.errorMessage = error.message
-  }
-
+  itemsBought.items = ShoppingListCheckOffService.getItems("itemsBought")
 }
 
 function ShoppingListCheckOffService(){
   let service = this
 
   let itemsTOBuyArray=[{
-    itemName:"Milk"
+    itemName:"Milk",
+    quantity: 10
   },
 {
-  itemName:"Chicken"
+  itemName:"Cookies",
+  quantity: 10
 },{
-  itemName: "water"
+  itemName: "water",
+  quantity: 10
+},{
+  itemName: "Choclate",
+  quantity: 5
+},{
+  itemName:"Rice",
+  quantity: 20
 }]
 
   let itemsBought = []
 
-  service.addItemBought = function(itemName){
+  service.addItemBought = function(itemName,itemQuantity,itemIndex){
     let item ={
-      itemName: itemName
+      itemName: itemName,
+      quantity: itemQuantity
     }
 
-    let indexNumber  = itemsBought.indexOf(item.itemName)
-    debugger
+
+    let indexNumber  = service.search(itemName, itemsBought)
+
+    
     if(indexNumber === -1){
-      itemsBought.push(item.itemName)
+      itemsBought.push(item)
+      itemsTOBuyArray.splice(itemIndex,1)
+
     }
     else{
-      debugger
+
       throw new Error("Item Already Bought")
     }
   }
@@ -75,6 +84,25 @@ function ShoppingListCheckOffService(){
           return itemsBought
       }
 
+    }
+  }
+
+  service.search= function(itemName, itemsBought){
+    if(itemsBought.length === 0){
+      return -1
+    }
+
+    for (let i=0; i < itemsBought.length; i++) {
+      if (itemsBought[i].itemName=== itemName) {
+          return i
+      }
+    }
+    return -1
+  }
+
+  service.checkEmpty = function(){
+    if(itemsTOBuyArray.length ===0){
+      throw new Error("Everything is Bought!")
     }
   }
 }
